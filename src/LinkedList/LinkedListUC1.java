0b122fd556d7130b2afe03ee1ff972fc1115b7cc
Devika18 <devikankarpe@gmail.com>
2022-10-18 23:30:36 +0530

    added: logic for creating linked list

---
+++ src/LinkedList/LinkedListUC1.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class LinkedListUC1 {
+    public static INode head;
+    public static INode tail;
+
+    public LinkedListUC1() {
+
+        this.head = null;
+        this.tail = null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to DataStructure : Linked List Program");
+        System.out.println(head);
+        System.out.println(tail);
+    }
+}
